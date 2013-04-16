fc610be759b1299e719f29bc5c6f29eb09238fb6
Gene Jones <gjones@keystonesolutions.com>
2013-04-15 20:12:32 -0400

    agent additions, ready for demo attempt

--- agent.c
+++ agent.c
@@ -3,9 +3,9 @@ function router(request, res)
 	try{
 		switch(request.path){
 			case 'param/TZ':
-				function set_tz(request, res);
+				set_tz(request, res);
 			case 'param/12':
-				function set_twelve(request, res);
+				set_twelve(request, res);
 			case 'alert':
 				//for emergency alerts
 				//not implemented yet
@@ -14,6 +14,7 @@ function router(request, res)
 				if (request.method == 'OPTIONS' && request.path == ''){
 					res.header('ALLOW', 'GET,PUT,DELETE,OPTIONS');
 					res.send(200, "OK");
+				}
 				res.send(400, "Bad Request");
 		}
 	}
@@ -23,9 +24,50 @@ function router(request, res)
 }
 
 function updatePermanent(name, value){
+	server.log('adding/changing ' +name + ' to ' + value);
 	cachedPerm = server.permanent;
 	cachedPerm <- {name = value};
 	server.setpermanentvalues(cachedPerm);
+}
+
+function alert(request, res){
+	if ('alertType' in request.query){
+		switch (request.method){
+			case 'PUT':
+				res.send(200, 'Not implemented yet, sorry');
+			case 'OPTIONS':
+				res.header('ALLOW', 'PUT,OPTIONS');
+				res.send(200, 'None of this is implemented yet. In the future, you can PUT a request with a secret key to allow for emergeny alerts');
+			default:
+				res.send(405, "Method not implemented. See HTTP OPTIONS for valid options.");
+		}
+	}
+	else{
+		throw("queryString Error");
+	}
+}
+
+function set_twelve(request, res){
+	switch (request.method){
+		case 'GET':
+			res.send(200, server.permanent.isTwelveHour);
+		case 'PUT':
+			local value = request.body.tointeger();
+			switch (value){
+				case '1':
+					updatePermanent('isTwelveHour', true);
+				case '0':
+					updatePermanent('isTwelveHour', false);
+				default:
+					throw("Not a valid value");
+			}
+			res.send(200, 'OK');
+		case 'OPTIONS':
+			res.header('ALLOW', 'PUT,GET,OPTIONS');
+			res.send(200, 'PUT or GET. Valid options are 1 or 0. 1 indicates 12 hour time, 0 indicates 24 hour time.');
+		default:
+			res.send(405, "Method not implemented. See HTTP OPTIONS for valid options.");
+	}
 }
 
 function set_tz(request, res){
@@ -33,24 +75,26 @@ function set_tz(request, res){
 		if (request.method == 'GET'){
 			res.send(200, server.permanent.tz_offset);
 		}
-		if (request.method == 'PUT' || request.method == ''){
+		if (request.method == 'PUT'){
 			offset = request.body.tointeger();
-			if (offset && offset.body>12 || request.body<14){
+			if (offset && offset>-12 && offset<14){
+				updatePermanent('tz_offset'=request.body);
+			}
+			else{
 				throw("offset incorrect");
 			}
-			updatePermanent('tz_offset'=request.body);
 			res.send(200, "OK");
 		}
 		if (request.method == 'OPTIONS'){
 			res.header('ALLOW', 'GET,PUT,OPTIONS');
-			res.send(200, "OK");
+			res.send(200, "Sets Timezone offset. Must be an integer between -12 and 14(sorry for any people living in .5 UTC offsets).");
 		}
 		else{
-			res.send(400, "Bad Request");
+			res.send(405, "Bad Request");
 		}
 	}	
 	else{
-		res.send(400, "Bad Request");
+		res.send(400, "Method not implemented. See HTTP OPTIONS for valid options.");
 	}
 }
 
